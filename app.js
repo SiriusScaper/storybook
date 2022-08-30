7ecc7fbfe26a399766bf4e2335576906e8e4ca90
siriusscaper <siriusscaper@gmail.com>
2022-08-30 15:33:54 -0700

    copied project structure and completed code from another project directory

---
+++ app.js
@@ -0,0 +1,88 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const {engine} = require('express-handlebars')
+const methodOverride = require('method-override')
+const passport = require('passport')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
+const connectDB = require('./config/db')
+const { default: mongoose } = require('mongoose')
+
+// Load config
+dotenv.config({path: './config/config.env'})
+
+// Passport Config
+require('./config/passport')(passport)
+
+connectDB()
+
+const app = express()
+
+//Body Parser
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+// Method override
+app.use(methodOverride(function (req, res){
+  if (req.body && typeof req.body === 'object' && '_method' in req.body){
+    // look in urlencoded POST bodies and delete it
+    let method = req.body._method
+    delete req.body._method
+    return method
+  }
+}))
+
+// Logging
+if (process.env.NODE_ENV === 'development'){
+  app.use(morgan('dev'))
+}
+
+//Handlebars Helpers
+const { formatDate, stripTags, truncate, editIcon, select } = require('./helpers/hbs')
+
+// Hnadlebars
+app.engine('.hbs', engine({
+  helpers: {
+    formatDate,
+    stripTags,
+    truncate,
+    editIcon,
+    select
+  },
+  extname: '.hbs'}))
+app.set('view engine', '.hbs')
+app.set('views', './views')
+
+// Sessions
+app.use(session({
+  secret: 'keyboard dog',
+  resave: false,
+  saveUninitialized: false,
+  store: MongoStore.create({ 
+    mongoUrl: process.env.MONGO_URI
+  })
+})
+)
+
+//Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+// Set global variable
+app.use(function (req, res, next) {
+  res.locals.user = req.user || null
+  next()
+})
+
+// Static Folder
+app.use(express.static('public'))
+
+// Routes
+app.use('/', require('./routes/index'));
+app.use('/auth', require('./routes/auth'));
+app.use('/stories', require('./routes/stories'));
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, console.log(`Server running ${process.env.NODE_ENV} mode on port ${PORT}`))
